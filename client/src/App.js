6a57cc2a196905557d096694c0fdd2cf04cfc8df
Nikhita <bnikhita2003@gmail.com>
2022-03-25 20:55:33 +0530

    uploaded header files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home, Dashboard, Profile } from './pages';
+import { Header } from './components';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/dashboard" element={<Dashboard />}/>
