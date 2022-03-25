5f8d214b2ef652a42abfbc0b6a7e6db697a1bf59
suvangipaul <suvangipaul8@gmail.com>
2022-03-25 20:12:30 +0530

    Codes Changed

--- client/src/pages/home/home.pages.jsx
+++ client/src/pages/home/home.pages.jsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
 
+import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
+import ThirdPartyEmailPassword, {
+    ThirdPartyEmailPasswordAuth,
+    Google,
+    Github,
+    Apple,
+} from "supertokens-auth-react/recipe/thirdpartyemailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+import Home from "./Home";
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+
+import SessionExpiredPopup from "./SessionExpiredPopup";
+
+export function getApiDomain() {
+    const apiPort = process.env.REACT_APP_API_PORT || 3001;
+    const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+    return apiUrl;
+}
+
+export function getWebsiteDomain() {
+    const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+    const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+    return websiteUrl;
+}
+
+SuperTokens.init({
+    appInfo: {
+        appName: "SuperTokens Demo App", // TODO: Your app name
+        apiDomain: getApiDomain(), // TODO: Change to your app's API domain
+        websiteDomain: getWebsiteDomain(), // TODO: Change to your app's website domain
+    },
+    recipeList: [
+        ThirdPartyEmailPassword.init({
+            signInAndUpFeature: {
+                providers: [Github.init(), Google.init(), Apple.init()],
+            },
+            emailVerificationFeature: {
+                mode: "REQUIRED",
+            },
+        }),
+        Session.init(),
+    ],
+});
 function Home() {
+  let [showSessionExpiredPopup, updateShowSessionExpiredPopup] = useState(false);
   return (
     <div>Home</div>
   )
