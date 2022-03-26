6a1cf9433014612cbf462032856c157d7fceddba
siddhantprateek <siddhantprateek@gmail.com>
2022-03-26 19:14:08 +0530

    updated footer

--- client/src/components/footer/footer.components.jsx
+++ client/src/components/footer/footer.components.jsx
@@ -6,39 +6,38 @@ function Footer() {
     <div class="l-footer">
     
     
-    <p>
-                                                                              Vibee</p>
+    <p>Vibee</p>
     </div>
     <ul class="r-footer">
     <li>
       <h2>
     Social</h2>
     <ul class="box">
-    <li><a href="#">Facebook</a></li>
-    <li><a href="#">Twitter</a></li>
-    <li><a href="#">Pinterest</a></li>
-    <li><a href="#">Dribbble</a></li>
+    <li><a href="/">Facebook</a></li>
+    <li><a href="/">Twitter</a></li>
+    <li><a href="/">Pinterest</a></li>
+    <li><a href="/">Dribbble</a></li>
     </ul>
     </li>
     <li class="features">
       <h2>
     Information</h2>
     <ul class="box h-box">
-    <li><a href="#">Blog</a></li>
-    <li><a href="#">Pricing</a></li>
-    <li><a href="#">Sales</a></li>
-    <li><a href="#">Tickets</a></li>
-    <li><a href="#">Certifications</a></li>
-    <li><a href="#">Customer Service</a></li>
+    <li><a href="/">Blog</a></li>
+    <li><a href="/">Pricing</a></li>
+    <li><a href="/">Sales</a></li>
+    <li><a href="/">Tickets</a></li>
+    <li><a href="/">Certifications</a></li>
+    <li><a href="/">Customer Service</a></li>
     </ul>
     </li>
     <li>
       <h2>
     Legal</h2>
     <ul class="box">
-    <li><a href="#">Privacy Policy</a></li>
-    <li><a href="#">Terms of Use</a></li>
-    <li><a href="#">Contract</a></li>
+    <li><a href="/">Privacy Policy</a></li>
+    <li><a href="/">Terms of Use</a></li>
+    <li><a href="/">Contract</a></li>
     </ul>
     </li>
     </ul>
