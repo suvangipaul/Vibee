8cb12c18a86d1f51939663ec0bc02ab205aff6ad
siddhantprateek <siddhantprateek@gmail.com>
2022-03-27 14:17:00 +0530

    server

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
-
-
+const dotenv = require('dotenv')
+dotenv.config()
 router.get('/api',(req, res) => {
-    res.json("api route")
+    res.json([{
+        "domain": process.env.DOMAIN,
+        "clientId": process.env.CLIENTID,
+        "audience": process.env.AUDIENCE
+    }])
 })
 
 
